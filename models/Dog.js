f931a2ce4c561af43669edcce667ab52ae1237ae
Coralee <45268389+Coralee-Long@users.noreply.github.com>
2022-02-17 19:56:16 +0100

    dog schema

--- models/Dog.js
+++ models/Dog.js
@@ -3,9 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Dog = new Schema({
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
   name: {
     type: String,
     required: true,
+    minLength: 1,
+    maxLength: 30,
+  },
+  breed: {
+    type: String,
     minLength: 1,
     maxLength: 30,
   },
@@ -17,10 +26,46 @@ const Dog = new Schema({
   age_months: {
     type: Number,
     min: 0,
-    max: 12,
+    max: 11,
   },
-  // size: {},
-  images: [String],
+  size: {
+    type: String,
+  },
+  profile_photo: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  energy: {
+    type: String,
+  },
+  kid_friendly: {
+    type: Number,
+  },
+  cat_friendly: {
+    type: Number,
+  },
+  dog_friendly: {
+    type: Number,
+  },
+  obedience: {
+    type: Number,
+  },
+  can_stay_home: {
+    type: Number,
+  },
+  exercise_type: {
+    type: Number,
+  },
+  can_play_fetch: {
+    type: Boolean,
+  },
+  photo_gallery: [String],
+
+  pd_counter: {
+    type: Number,
+  },
 });
 
 module.exports = mongoose.model("Dog", Dog);
