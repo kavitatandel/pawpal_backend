52ebb79748bd9a381ba5d1fb0214c6b6e11dd3c1
kavitatandel <kavita2810@gmail.com>
2022-02-18 17:36:30 +0100

    updated userContorller and userRouter

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const usersRouter = express.Router();
 const User = require("../models/User");
-const { getUsers, getAllUsers } = require("../controllers/userController");
+const { getUsers,
+    getAllUsers,
+    updateUser,
+} = require("../controllers/userController");
 
 // Get all Users (Owners , Doglovers , Admins)
 usersRouter.get("/users", getUsers);
 
 // Get One User (Owner / Doglover / Admin)
-usersRouter.get("/users/:id", getUsers);
+// usersRouter.get("/users/:id", getUsers);
+
+//update user infos
+usersRouter.post('/users/:id', updateUser);
 
 module.exports = usersRouter;
