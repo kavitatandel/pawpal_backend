99f210eaaed542bf06b1dd76f0e9da475fd561dc
kavitatandel <kavita2810@gmail.com>
2022-02-24 18:36:15 +0100

    updated dog controller and router for profile pic

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -12,35 +12,76 @@ const getDogs = async (req, res, next) => {
 }
 
 //insert dogs by user id
-const addDog = (req, res) => {
-    try {
-        const newDog = new Dog({
-            user_id: req.body.user_id,
-            name: req.body.name,
-            breed: req.body.breed,
-            age_years: req.body.age_years,
-            age_months: req.body.age_months,
-            size: req.body.size,
-            //profile_photo: req.body.profile_photo,
-            description: req.body.description,
-            energy: req.body.energy,
-            kid_friendly: req.body.kid_friendly,
-            cat_friendly: req.body.cat_friendly,
-            dog_friendly: req.body.dog_friendly,
-            obedience: req.body.obedience,
-            can_stay_home: req.body.can_stay_home,
-            exercise_type: req.body.exercise_type,
-            can_play_fetch: req.body.can_play_fetch,
-            // photo_gallery: req.body.photo_gallery,
-            // pd_counter: req.body.pd_counter
-        });
+// const addDog = (req, res, next) => {
+//     try {
 
-        newDog.save();
-        res.send(`Dog is added`);
+//         const newDog = new Dog({
+//             user_id: req.body.user_id,
+//             name: req.body.name,
+//             breed: req.body.breed,
+//             age_years: req.body.age_years,
+//             age_months: req.body.age_months,
+//             size: req.body.size,
+//             // profile_photo: url, //added for dog pic
+//             description: req.body.description,
+//             energy: req.body.energy,
+//             kid_friendly: req.body.kid_friendly,
+//             cat_friendly: req.body.cat_friendly,
+//             dog_friendly: req.body.dog_friendly,
+//             obedience: req.body.obedience,
+//             can_stay_home: req.body.can_stay_home,
+//             exercise_type: req.body.exercise_type,
+//             can_play_fetch: req.body.can_play_fetch,
+//             // photo_gallery: req.body.photo_gallery,
+//             // pd_counter: req.body.pd_counter
+//         });
+
+//         newDog.save();
+//         res.send(`Dog is added`);
+//     }
+//     catch (err) {
+//         res.status(404).send(err);
+//     }
+// }
+
+
+const addDog = (req, res, next) => {
+    try {
+        //get uploaded file
+        if (!req.file) {
+            next(new Error("No file uploaded!"));
+            return;
+        }
+        else {
+            const newDog = new Dog({
+                user_id: req.body.user_id,
+                name: req.body.name,
+                breed: req.body.breed,
+                age_years: req.body.age_years,
+                age_months: req.body.age_months,
+                size: req.body.size,
+                profile_photo: req.file.path, //added for dog pic
+                description: req.body.description,
+                energy: req.body.energy,
+                kid_friendly: req.body.kid_friendly,
+                cat_friendly: req.body.cat_friendly,
+                dog_friendly: req.body.dog_friendly,
+                obedience: req.body.obedience,
+                can_stay_home: req.body.can_stay_home,
+                exercise_type: req.body.exercise_type,
+                can_play_fetch: req.body.can_play_fetch,
+                // photo_gallery: req.body.photo_gallery,
+                // pd_counter: req.body.pd_counter
+            });
+
+            newDog.save();
+            res.send(`Dog is added`);
+        }
     }
     catch (err) {
         res.status(404).send(err);
     }
+
 }
 
 //insert dogs by user id
