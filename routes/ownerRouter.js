2b300da54ff12d4af12b4f52f132f44ca8ad8184
kavitatandel <kavita2810@gmail.com>
2022-02-28 00:30:27 +0100

    added UpdatePlayDateRequest

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -4,9 +4,10 @@ const User = require("../models/User");
 const Dog = require("../models/Dog");
 const Request = require("../models/Request");
 
-const { GetPlayDateRequestsForOwner,
+const { GetPlayDateRequestsForOwner, UpdatePlayDateRequest
 } = require("../controllers/ownerController");
 
 ownerRouter.get('/GetPlayDateRequestsForOwner', GetPlayDateRequestsForOwner);
+ownerRouter.post('/UpdatePlayDateRequest', UpdatePlayDateRequest);
 
 module.exports = ownerRouter;
